6b217ae7d71711397c459491b7a591d27b91ebb2
unsnruu <unsnruu@gmail.com>
2022-06-29 11:06:51 +0900

    Add Links to each project

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Link to="/counter">Counter</Link>
+      <Link to="/dragndrop">drag and drop</Link>
+    </div>
+  );
 }
 
 export default Home;
