ca1ee856ab05aa72465261d335ae56f7c3aba3d6
unsnruu <unsnruu@gmail.com>
2022-06-29 11:04:00 +0900

    Init Drag-And-Drop project

---
+++ src/features/drag-and-drop/DragAndDrop.tsx
@@ -0,0 +1,5 @@
+function DragAndDrop() {
+  return <div></div>;
+}
+
+export default DragAndDrop;
---
+++ src/routes/DragAndDropRoute.tsx
@@ -0,0 +1,7 @@
+import DragAndDrop from "../features/drag-and-drop/DragAndDrop";
+
+function DragAndDropRoutes() {
+  return <DragAndDrop />;
+}
+
+export default DragAndDropRoutes;
