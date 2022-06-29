e3e5c0fa72f72e9b035fa5b61b00b319e6aea3ff
unsnruu <unsnruu@gmail.com>
2022-06-29 11:01:54 +0900

    Add Routes

--- src/routes/IndexRouter.tsx
+++ src/routes/IndexRouter.tsx
@@ -1,5 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+
+import Home from "./Home";
+import CounterRoute from "./Counter";
+import DragAndDropRoutes from "./DragAndDropRoute";
+
 function IndexRouter() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/counter" element={<CounterRoute />}></Route>
+      <Route path="/dragndrop" element={<DragAndDropRoutes />}></Route>
+    </Routes>
+  );
 }
 
 export default IndexRouter;
