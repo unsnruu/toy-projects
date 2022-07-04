82e604d8d8d23e9285b099f38631556b4daa2f9a
unsnruu <unsnruu@gmail.com>
2022-07-04 11:59:07 +0900

    Add new elements

--- src/features/template-maker/EditingInnterElement.tsx
+++ src/features/template-maker/EditingInnterElement.tsx
@@ -1,3 +1,5 @@
+import styled from "@emotion/styled";
+
 interface EditingInnerElementProps {
   isExpanded: boolean;
   content: string;
@@ -10,9 +12,31 @@ function EditingInnerElement({
   return (
     <div>
       <div>{content}</div>
-      {isExpanded ? <div>Expanded</div> : null}
+      {isExpanded ? <ExpandedElement /> : null}
     </div>
   );
 }
 
 export default EditingInnerElement;
+
+function ExpandedElement() {
+  return <div>여기에 컴포넌트가 추가됩니다</div>;
+}
+
+function TextElement() {
+  return <input type="text" />;
+}
+function ImageElement() {
+  return <input type="file" />;
+}
+function ButtonElement() {
+  return <input type="button" />;
+}
+
+const Divider = styled.div`
+  height: 2px;
+  border: 1px solid blakc;
+`;
+function DividerElement() {
+  return <Divider />;
+}
