04c20b6d8cdb2b2ca0908dac82079c6b2b1e775a
unsnruu <unsnruu@gmail.com>
2022-07-07 16:16:13 +0900

    Fix css props

--- src/features/template-maker/ElementController.tsx
+++ src/features/template-maker/ElementController.tsx
@@ -6,20 +6,22 @@ import { ControllerEventHandlers } from "./Container";
 import prevRoute from "./icons/controller/prev.png";
 import nextRoute from "./icons/controller/next.png";
 import deleteRoute from "./icons/controller/delete.png";
-import moveRoute from "./icons/controller/move.png";
 import copyRoute from "./icons/controller/copy.png";
 
 const Side = styled.div`
   position: absolute;
+  z-index: 0;
   opacity: 0;
 `;
 const Wrapper = styled.div`
   position: relative;
+  z-index: 99;
   &:hover div {
     opacity: 1;
   }
 `;
 const ControllerIcon = styled.div`
+  cursor: pointer;
   & img {
     width: 1.5rem;
     height: 1.5rem;
@@ -39,16 +41,18 @@ function ElementController({
   return (
     <Wrapper>
       <Side style={{ left: 0 }}>
-        <ControllerIcon>
-          <img src={moveRoute} alt="move" draggable="false" />
-        </ControllerIcon>
         <ControllerIcon
           onClick={contorllerEventHandlers.createClickPrevHandler(id)}
         >
           <img src={prevRoute} alt="prev" draggable="false" />
         </ControllerIcon>
         <ControllerIcon>
-          <img src={nextRoute} alt="next" draggable="false" />
+          <img
+            src={nextRoute}
+            alt="next"
+            draggable="false"
+            onClick={contorllerEventHandlers.createClickNextHandle(id)}
+          />
         </ControllerIcon>
       </Side>
       <Side style={{ right: 0 }}>
@@ -56,7 +60,12 @@ function ElementController({
           <img src={copyRoute} alt="copy" draggable="false" />
         </ControllerIcon>
         <ControllerIcon>
-          <img src={deleteRoute} alt="delete" draggable="false" />
+          <img
+            src={deleteRoute}
+            alt="delete"
+            draggable="false"
+            onClick={contorllerEventHandlers.createClickDeleteHandle(id)}
+          />
         </ControllerIcon>
       </Side>
       {children}
