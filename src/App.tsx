1cfeaed090f7d9f38520116b8465c155f7802483
unsnruu <unsnruu@gmail.com>
2022-10-01 13:42:39 +0900

    🚚 import 구문의 path 수정 (#1)
    import 구문의 path를 features/. 에서 pages/. 로 수정하였습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/Home";
-import DnDContainer from "./features/drag-and-drop/DnDContainer";
-import TemplateMaker from "./features/template-maker/Container";
-import AutoBanner from "./features/auto-banner/Container";
+import DnDContainer from "./pages/drag-and-drop/DnDContainer";
+import TemplateMaker from "./pages/template-maker/Container";
+import AutoBanner from "./pages/auto-banner/Container";
 import NotFound from "./routes/NotFound";
-import Switch from "./features/switch/Swtich";
-import DraggableBar from "./features/draggable-bar/Container";
+import Switch from "./pages/switch/Swtich";
+import DraggableBar from "./pages/draggable-bar/Container";
 
 function App() {
   return (
